dd6f552b578d936772ff95e5e952ae17bcf40c5c
Satoshi OOMORI <oogon@mac.com>
2012-03-24 14:29:27 +0900

    Cocoa workshop 201203
    Cocoa勉強会　UITestの自動化サンプル

---
+++ cocoa_workshop/testTrace/testTrace/OOOAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  OOOAppDelegate.h
+//  testTrace
+//
+//  Created by 大森 智史 on 12/01/21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface OOOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cocoa_workshop/testTrace/testTraceTests/testTraceTests.h
@@ -0,0 +1,13 @@
+//
+//  testTraceTests.h
+//  testTraceTests
+//
+//  Created by 大森 智史 on 12/01/21.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTraceTests : SenTestCase
+
+@end
