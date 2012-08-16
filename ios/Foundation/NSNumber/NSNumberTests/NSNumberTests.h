09f07c5fdd4450c531dc6c4f382da0ea49d75f50
Satoshi OOMORI <oogon@mac.com>
2012-08-16 16:01:38 +0900

    NSNumber

---
+++ ios/Foundation/NSNumber/NSNumberTests/NSNumberTests.h
@@ -0,0 +1,13 @@
+//
+//  NSNumberTests.h
+//  NSNumberTests
+//
+//  Created by 大森 智史 on 2012/08/16.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSNumberTests : SenTestCase
+
+@end
