b633bf8303d9afda9c85ca2fcb0ea5bf1544b498
Satoshi OOMORI <oogon@mac.com>
2012-08-16 16:06:44 +0900

    NSDataDetector

---
+++ ios/Foundation/NSDataDetector/NSDataDetectorTests/NSDataDetectorTests.h
@@ -0,0 +1,13 @@
+//
+//  NSDataDetectorTests.h
+//  NSDataDetectorTests
+//
+//  Created by 大森 智史 on 2012/08/15.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSDataDetectorTests : SenTestCase
+
+@end
---
+++ ios/Foundation/NSDataDetector/NSDataDetectorTests/NSDataDetectorTests.m
@@ -0,0 +1,32 @@
+//
+//  NSDataDetectorTests.m
+//  NSDataDetectorTests
+//
+//  Created by 大森 智史 on 2012/08/15.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import "NSDataDetectorTests.h"
+
+@implementation NSDataDetectorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSDataDetectorTests");
+}
+
+@end
