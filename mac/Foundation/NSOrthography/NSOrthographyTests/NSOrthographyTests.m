8a4c60651bf61754eeedc6dc34f53724bd8c21d8
Satoshi OOMORI <oogon@mac.com>
2012-08-16 16:00:10 +0900

    8/16

---
+++ mac/Foundation/NSOrthography/NSOrthographyTests/NSOrthographyTests.m
@@ -0,0 +1,32 @@
+//
+//  NSOrthographyTests.m
+//  NSOrthographyTests
+//
+//  Created by 大森 智史 on 2012/08/14.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import "NSOrthographyTests.h"
+
+@implementation NSOrthographyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSOrthographyTests");
+}
+
+@end
