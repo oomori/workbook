12e7d2ee00c27b882c1306b44940680b31d35494
Satoshi OOMORI <oogon@mac.com>
2012-08-16 16:06:10 +0900

    NSDate

---
+++ ios/Foundation/NSDate/NSDateTests/NSDateTests.m
@@ -0,0 +1,32 @@
+//
+//  NSDateTests.m
+//  NSDateTests
+//
+//  Created by 大森 智史 on 2012/08/15.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import "NSDateTests.h"
+
+@implementation NSDateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSDateTests");
+}
+
+@end
