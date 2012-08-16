e8222d07112414fbf006537c002139739912d075
Satoshi OOMORI <oogon@mac.com>
2012-08-16 16:02:31 +0900

    NSNumber

---
+++ mac/Foundation/NSNumber/NSNumber/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NSNumber
+//
+//  Created by 大森 智史 on 2012/08/16.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
