528472f3dec5bb3b2604ff77436acbeeb30ba6af
Satoshi OOMORI <oogon@mac.com>
2012-08-16 16:05:25 +0900

    NSMutableCharacterSet

---
+++ ios/Foundation/NSMutableCharacterSet/NSMutableCharacterSetTests/NSMutableCharacterSetTests.m
@@ -0,0 +1,32 @@
+//
+//  NSMutableCharacterSetTests.m
+//  NSMutableCharacterSetTests
+//
+//  Created by 大森 智史 on 2012/08/14.
+//  Copyright (c) 2012年 Satoshi Oomori. All rights reserved.
+//
+
+#import "NSMutableCharacterSetTests.h"
+
+@implementation NSMutableCharacterSetTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSMutableCharacterSetTests");
+}
+
+@end
