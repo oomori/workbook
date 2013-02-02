9695bbd64ce14ed7b7c626028dad90c28e683907
Satoshi OOMORI <oogon@mac.com>
2013-02-02 14:56:36 +0900

    20120202
    Cocoa勉強会発表NSStringDrawing

--- ios/etc/NSStringDrawing/NSStringDrawing/OOOView.m
+++ ios/etc/NSStringDrawing/NSStringDrawing/OOOView.m
@@ -11,6 +11,8 @@
 #import "OOOLayer.h"
 #import "OOOView.h"
 
+#import <CoreText/CoreText.h>
+
 @implementation OOOView
 
 - (id)initWithFrame:(CGRect)frame
@@ -31,25 +33,71 @@
 {
 
     //属性付き文字列作成
-        NSMutableAttributedString *aString = [[NSMutableAttributedString alloc] initWithString:@"string"];
-        
-        NSLog(@"%s %@",__FUNCTION__,[aString description]);
-        
-        [aString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(0, 2)];
-        [aString addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(3, 1)];
-        
-        [aString enumerateAttribute:NSForegroundColorAttributeName inRange:NSMakeRange(0, 5)
-         
-                            options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired
-                         usingBlock:^(id value, NSRange range, BOOL *stop)
-         {
-             UIColor *fontColor = value;
-             NSLog(@"%@",[fontColor description]);
-         }
-         ];
-    //描画
-    [aString drawInRect:CGRectMake(10, 10, 100, 100)];
+    UIFont *font = [UIFont fontWithName:@"HiraMinProN-W6" size:40.0];
+
+    //NSDictionary *fontAttributes = @{@"NSFont": font};
+
+//影
+    NSShadow *shadowDic=[[NSShadow alloc] init];
+    [shadowDic setShadowBlurRadius:5.0];
+    [shadowDic setShadowColor:[UIColor grayColor]];
+    [shadowDic setShadowOffset:CGSizeMake(0, 3)];
     
+    NSDictionary *fontAttributes = @{@"NSFont": font,
+                                        NSShadowAttributeName:shadowDic,
+                                        //@"NSVerticalGlyphFormAttributeName":@YES
+                                        @"CTVerticalForms": @YES
+                                        };
+    
+
+    
+    NSMutableAttributedString *aString = [[NSMutableAttributedString alloc] initWithString:@"文字ですよー"  attributes:fontAttributes];
+
+    
+//属性付き文字の詳細
+//    NSLog(@"%s %@",__FUNCTION__,[aString description]);
+    
+    
+//1属性追加
+    [aString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(0, 2)];//0番目から2文字を赤にする
+    [aString addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(4, 1)];//4番目から1文字をブルーにする
+    
+    //描画    
+    [aString drawInRect:CGRectMake(10, 10, 200, 100)];
+
+    
+    
+    
+    
+    
+    
+    
+    
+//    NSStringDrawingContext * context = [NSStringDrawingContext alloc];
+//    context.minimumTrackingAdjustment = -0.5;
+//    [aString drawWithRect:CGRectMake(10, 10, 200, 100)
+//                  options:NSStringDrawingUsesLineFragmentOrigin context:context ];
+    
+    
+//アトリビュート描画
+//    [aString enumerateAttribute:NSForegroundColorAttributeName inRange:NSMakeRange(0, 5)
+//     
+//                        options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired
+//                     usingBlock:^(id value, NSRange range, BOOL *stop)
+//     {
+//         UIColor *fontColor = value;
+//         NSLog(@"%@",[fontColor description]);
+//     }
+//     ];
+    
+    
+    
+    
+    
+    
+    
+    
+    /*テスト中
     OOOLayer* subLayer = [OOOLayer layer];
     subLayer.frame = self.frame;
     [self.layer addSublayer:subLayer];
@@ -70,7 +118,7 @@
     
     //self.layer.position = CGPointMake(100, 100);
     //self.layer.sublayerTransform = t;
-    
+    */
 }
 
 
